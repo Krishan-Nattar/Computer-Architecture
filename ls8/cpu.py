ea6b7c8a97beb14c67c0a9978566febe76384484
Krishan Nattar <krishan.nattar@gmail.com>
2020-02-24 19:14:45 -0800

    CALL and RET working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,7 @@ POP = 0b01000110 # Pop instruction off the stack
 PUSH = 0b01000101 # Push instruction onto the stack
 CALL = 0b01010000 # Jump to a subroutine's address
 RET = 0b00010001 # Go to return address after subroutine is done
+ADD = 0b10100000 # Add two registry addresses together
 
 class CPU:
     """Main CPU class."""
@@ -27,45 +28,106 @@ class CPU:
         self.branchtable[MUL] = self.handle_MUL
         self.branchtable[POP] = self.handle_POP
         self.branchtable[PUSH] = self.handle_PUSH
+        self.branchtable[CALL] = self.handle_CALL
+        self.branchtable[RET] = self.handle_RET
+        self.branchtable[ADD] = self.handle_ADD
 
         self.register[7] = 0xF4 # initialized to point at key press
 
-    def handle_POP(self):
+    def handle_CALL(self):
+        '''
+        Set the PC to the address of a called subroutine.
+        Save the address of the operation that follows CALL in the stack
+        '''
+
+        # Push that onto the stack
+        self.handle_PUSH(True)
+
+
+    def handle_RET(self):
+        '''
+        Pop saved PC address off stack and move PC to that location
+        continue operations from there
+        '''
+        
+        self.handle_POP(True)
+
+    def handle_POP(self, ret = False):
+        '''
+        Copy value of most recently pushed stack item to given registry address.
+        Move stack pointer to previously pushed stack item
+        '''
         SP = self.register[7]
         value = self.ram[SP]
-        reg = self.ram_read(self.pc + 1)
-        self.register[reg] = value
-        self.register[7] += 1
-        self.pc += 2
 
-    def handle_PUSH(self):
+        if not ret:
+            reg = self.ram_read(self.pc + 1)
+            self.register[reg] = value
+            # self.register[7] += 1
+            self.pc += 2
+        else:
+            # SP = self.register[7]
+            # value = self.ram[SP] # address of where to return to
+            self.pc = value # Move PC back to the next operation after the CALL
+
+        self.register[7] += 1
+
+    def handle_PUSH(self, call = False):
+        '''
+        Push value from given registry address onto next stack. 
+        Leave stack pointer at most recently pushed value
+        If call == True, store value of next operation and set pointer to the address saved in the next byte
+        '''
         self.register[7] -= 1
         SP = self.register[7]
-        reg = self.ram_read(self.pc + 1)
-        self.ram_write(self.register[reg], SP)
-        self.pc += 2
 
+        if not call:
+            reg = self.ram_read(self.pc + 1)
+            value = self.register[reg]
+            self.pc += 2
+        else:
+            value = self.pc + 2
+            self.pc = self.register[self.ram_read(self.pc + 1)]
 
+        self.ram_write(value, SP)
 
     def handle_LDI(self):
+        '''
+        Given a registry address and a number, put number into registry.
+        '''
         reg = self.ram_read(self.pc + 1)
         num = self.ram_read(self.pc + 2)
         self.register[reg] = num
         self.pc += 3
 
     def handle_PRN(self):
+        '''
+        Given a registry address, print number value that exists there
+        '''
         reg = self.ram_read(self.pc + 1)
         num = self.register[reg]
         print(num)
         self.pc += 2
 
     def handle_HLT(self):
+        '''
+        Halt Program
+        '''
         sys.exit(1)
 
     def handle_MUL(self):
+        '''
+        Given two registry addresses, 
+        multiply them together and save the value in the first registry
+        '''
         operand_a = self.ram_read(self.pc + 1)
         operand_b = self.ram_read(self.pc + 2)
         self.alu("MUL", operand_a, operand_b)
+        self.pc += 3
+    def handle_ADD(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("ADD", operand_a, operand_b)
         self.pc += 3
 
     def load(self):
@@ -120,8 +182,14 @@ class CPU:
             self.branchtable[IR]()
 
     def ram_read(self, MAR):
+        '''
+        Read a value from a given ram index
+        '''
         return self.ram[MAR]
 
     def ram_write(self, MDR, MAR):
+        '''
+        Write a value into a given ram index
+        '''
         self.ram[MAR] = MDR
 
