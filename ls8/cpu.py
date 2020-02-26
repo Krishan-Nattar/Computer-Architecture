e72be94210d21a0466e98b12be5d521c9d8e795d
Krishan Nattar <krishan.nattar@gmail.com>
2020-02-26 13:50:58 -0800

    working on timer interrupt

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,16 +1,61 @@
 """CPU functionality."""
 
 import sys
+from datetime import datetime, timedelta
+import time
 
-LDI = 0b10000010 # Set a specific register to a specific value
-PRN = 0b01000111 # Print a number
-HLT = 0b00000001 # Halt the program
+# ALU OPS
+
+ADD = 0b10100000 # Add two registry addresses together
+SUB = 0b10100001
 MUL = 0b10100010 # Multiply two registers together and store result in register A
-POP = 0b01000110 # Pop instruction off the stack
-PUSH = 0b01000101 # Push instruction onto the stack
+DIV = 0b10100011
+MOD = 0b10100100
+
+INC = 0b01100101
+DEC = 0b01100110 
+
+CMP = 0b10100111
+
+AND = 0b10101000
+NOT = 0b01101001
+OR = 0b10101010
+XOR = 0b10101011 
+SHL = 0b10101100
+SHR = 0b10101101 
+
+# PC MUTATORS
+
 CALL = 0b01010000 # Jump to a subroutine's address
 RET = 0b00010001 # Go to return address after subroutine is done
-ADD = 0b10100000 # Add two registry addresses together
+
+INT = 0b01010010
+IRET = 0b00010011 # Interrupt Return
+
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
+JGT = 0b01010111
+JLT = 0b01011000
+JLE = 0b01011001
+JGE = 0b01011010
+
+# OTHER
+
+NOP = 0b00000000
+
+HLT = 0b00000001 # Halt the program
+
+LDI = 0b10000010 # Set a specific register to a specific value
+
+LD = 0b10000011
+ST = 0b10000100 # Store value from registerB in the address stored in registerA
+
+PUSH = 0b01000101 # Push instruction onto the stack
+POP = 0b01000110 # Pop instruction off the stack
+
+PRN = 0b01000111 # Print a number
+PRA = 0b01001000
 
 class CPU:
     """Main CPU class."""
@@ -18,9 +63,14 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.register = [0] * 8
+        # R5 = IM = Interrupt Mask
+        # R6 = IS = Interrupt Status
+        # R7 = SP = Stack Pointer
+
         self.ram = [0] * 255
         self.pc = 0
         self.branchtable = {}
+        
         
         self.branchtable[LDI] = self.handle_LDI
         self.branchtable[PRN] = self.handle_PRN
@@ -31,8 +81,56 @@ class CPU:
         self.branchtable[CALL] = self.handle_CALL
         self.branchtable[RET] = self.handle_RET
         self.branchtable[ADD] = self.handle_ADD
+        self.branchtable[ST] = self.handle_ST
+        self.branchtable[IRET] = self.handle_IRET
+        self.branchtable[JMP] = self.handle_JMP
 
-        self.register[7] = 0xF4 # initialized to point at key press
+        self.sp = 7
+        self.register[self.sp] = 0xF4 # initialized to point at key press
+        # self.time = datetime.now().time()
+        # print(datetime.now())
+        self.time = time.time()
+        self.interrupt_handler_address = 0xf8
+        self.interrupt_mask = 5
+        self.interrupt_status = 6
+        self.interrupts_enabled = True
+    def handle_JMP(self):
+        '''
+        Jump to the address stored in the given register.
+        Set the PC to the address stored in the given register.
+        '''
+        set_pointer = self.ram_read(self.pc + 1)
+        self.pc = set_pointer
+
+        pass
+    def handle_IRET(self):
+        '''
+        Handle Interrupt return
+        '''
+        # While an interrupt is being serviced (between the handler being called and the IRET), further interrupts are disabled.
+
+        # Return from an interrupt handler.
+        # The following steps are executed:
+
+
+        # Registers R6-R0 are popped off the stack in that order.
+
+        # The FL register is popped off the stack.
+
+        # The return address is popped off the stack and stored in PC.
+
+        # Interrupts are re-enabled
+        self.interrupts_enabled = True
+        pass
+
+    def handle_ST(self):
+        '''
+        Take the value from registerB and store in the ADDRESS stored in registerB
+        '''
+        regA = self.register[self.pc+1]
+        regB = self.register[self.pc + 2]
+        self.ram[regA] = self.register[regB]
+        self.pc += 3
 
     def handle_CALL(self):
         '''
@@ -42,7 +140,6 @@ class CPU:
 
         # Push that onto the stack
         self.handle_PUSH(True)
-
 
     def handle_RET(self):
         '''
@@ -57,17 +154,17 @@ class CPU:
         Move stack pointer to previously pushed stack item
         if ret = True, set PC to the popped value and don't save into register
         '''
-        SP = self.register[7]
+        SP = self.register[self.sp]
         value = self.ram[SP]
 
         if not ret:
             reg = self.ram_read(self.pc + 1)
             self.register[reg] = value
             self.pc += 2
-        else:
+        else: # RET POP
             self.pc = value # Move PC back to the next operation after the CALL
 
-        self.register[7] += 1
+        self.register[self.sp] += 1
 
     def handle_PUSH(self, call = False):
         '''
@@ -75,14 +172,14 @@ class CPU:
         Leave stack pointer at most recently pushed value
         If call == True, store value of next operation and set pointer to the address saved in the next byte
         '''
-        self.register[7] -= 1
-        SP = self.register[7]
+        self.register[self.sp] -= 1
+        SP = self.register[self.sp]
 
         if not call:
             reg = self.ram_read(self.pc + 1)
             value = self.register[reg]
             self.pc += 2
-        else:
+        else: # CALL PUSH
             value = self.pc + 2
             self.pc = self.register[self.ram_read(self.pc + 1)]
 
@@ -138,11 +235,10 @@ class CPU:
                 if line[0] != "#" and line !='\n':
                     self.ram[address] = int(line[:8], 2)
                     address += 1
-        # print(self.ram)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
+        # Can set up ALU branchtable
         if op == "ADD":
             self.register[reg_a] += self.register[reg_b]
         elif op == "MUL":
@@ -167,7 +263,7 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
@@ -175,6 +271,91 @@ class CPU:
         """Run the CPU."""
         
         while True:
+
+            # Prior to instruction fetch, the following steps occur:
+
+            #DONE # The IM register is bitwise AND-ed with the IS register and the results stored as maskedInterrupts.
+            
+            #DONE # Each bit of maskedInterrupts is checked, starting from 0 and going up to the 7th bit, one for each interrupt.
+            
+            # TODO
+            # If a bit is found to be set, follow the next sequence of steps. 
+            
+            # DONE # Stop further checking of maskedInterrupts.
+            
+            '''
+            Add the timer interrupt to the LS-8 emulator
+            '''
+
+            '''
+            Interrupt numbers
+            0: Timer interrupt. This interrupt triggers once per second.
+            1: Keyboard interrupt. This interrupt triggers when a key is pressed. The value of the key pressed is stored in address 0xF4.
+            '''
+            # Do we ever need self.interrupts_enabled to actually be False???
+            if self.interrupts_enabled:
+                current_time = time.time()
+                if current_time >= 1:
+                    self.time = time.time() # Reset base time to check against
+
+                    # R6 is reserved for the interrupt_status
+                    self.register[self.interrupt_status] = 1 # Interrupt status is on? (0b00000001)
+
+
+                    # TEMP CODE ***********
+                    # LDI WILL CREATE THIS FOR US
+                    # R5 is our Interrupt Mask slot
+                    self.register[self.interrupt_mask] = 0b00000001
+                    # TEMP CODE **********
+
+                    
+                # If interrupt mask is on and interrupt status is on, this will evaluate to 1
+                masked_interrupts = self.register[self.interrupt_mask] & self.register[self.interrupt_status]
+
+                print(f"Masked interrupts is {masked_interrupts}")
+                
+                # Check if interrupt status is switched on
+                for i in range(8):
+                    # Right shift interrupts down by i, then mask with 1 to see if that bit was set
+                    interrupt_happened = ((masked_interrupts >> i) & 1) == 1
+
+                    # If a bit is set:
+                    if interrupt_happened:
+                        print(f"Interrupt Happened: {interrupt_happened} i: {i}")
+                        # Interrupt can really only happen in i = 1, so why the others?!
+
+                        # Disable further interrupts.
+                        self.interrupts_enabled = False
+
+                        # Clear the bit in the IS register.
+                        # Reset Interrupt status to 0
+                        self.register[self.interrupt_status] = 0
+
+
+
+                        '''
+                        ***With interrupts, why do we push everything on the stack?***
+
+                        The idea is that if you save the machine state on the stack, then after you service the interrupt you can restore it and seamlessly pick up where you left off.
+
+                        The CPU might have been in the middle of something important when the interrupt occurred, and it'll want to get back to that once the interrupt handler is complete.
+
+                        So we push the general purpose registers and internal registers on the stack, then do interrupt stuff, then restore all those registers from the stack so the CPU can carry on with what it was doing before the interrupt occurred.
+                        '''
+                        # TODO
+                        # The PC register is pushed on the stack.
+                        # The FL register is pushed on the stack.
+                        # Registers R0-R6 are pushed on the stack in that order.
+                        # The address (vector in interrupt terminology) of the appropriate handler is looked up from the interrupt vector table.
+                        # Set the PC is set to the handler address.
+
+
+                        # Stop further checking of maskedInterrupts.
+                        break
+            else:
+                pass
+
+            # # self.trace()
             IR = self.ram[self.pc]
             self.branchtable[IR]()
 
