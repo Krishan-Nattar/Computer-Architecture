b2d87d7a024b862b65367f830f63c6babb83cf3d
Krishan Nattar <krishan.nattar@gmail.com>
2020-02-26 17:21:44 -0800

    prepping for histogram

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,6 +3,8 @@
 import sys
 from datetime import datetime, timedelta
 import time
+import msvcrt 
+
 
 # ALU OPS
 
@@ -87,6 +89,11 @@ class CPU:
         self.branchtable[JMP] = self.handle_JMP
         self.branchtable[PRA] = self.handle_PRA
         self.branchtable[NOP] = self.handle_NOP
+        self.branchtable[LD] = self.handle_LD
+        self.branchtable[CMP] = self.handle_CMP
+        self.branchtable[JLT] = self.handle_JLT
+        self.branchtable[JNE] = self.handle_JNE
+        self.branchtable[JEQ] = self.handle_JEQ
 
         self.sp = 7
         self.register[self.sp] = 0xF4 # initialized to point at key press
@@ -94,9 +101,90 @@ class CPU:
         self.interrupt_mask = 5
         self.interrupt_status = 6
         self.interrupts_enabled = True
+
+        self.init_time = 0
+    def kbfunc(self): 
+        '''
+        Catches a keyboard input.
+        Saves that value as a number in ram
+        Adds 2 to the interrupt status
+        '''
+        x = msvcrt.kbhit()
+        if x: 
+            # Creates a number associated with the keyboard input
+            ret = ord(msvcrt.getch()) 
+            self.ram[0xf4] = ret
+
+            # Adding to the register instead of overwriting in case there are other interrupt statuses being created
+            self.register[self.interrupt_status] += 0b00000010
     
+    def handle_CMP(self):
+        pass
+
+    def handle_JLT(self):
+        pass
+
+    def handle_JNE(self):
+        pass
+
+    def handle_JEQ(self):
+        pass
+
+    def handle_interrupt(self):
+        current_time = time.time()
+        if current_time - self.init_time >=1:
+
+            self.init_time = time.time() # Reset base time to check against
+
+            # R6 is reserved for the interrupt_status
+            self.register[self.interrupt_status] += 0b00000001 # Interrupt status is on? (0b00000001)
+
+        # If interrupt mask is on and interrupt status is on, this will evaluate to 1
+        masked_interrupts = self.register[self.interrupt_mask] & self.register[self.interrupt_status]
+
+        # Check if interrupt status is switched on
+        for i in range(8):
+            # Right shift interrupts down by i, then mask with 1 to see if that bit was set
+            interrupt_happened = ((masked_interrupts >> i) & 1) == 1
+            # if interrupt_happened:
+
+            # If a bit is set:
+            if interrupt_happened:
+                # Based on the type of interrupt, we set the interrupt handler the relevant ram address
+                self.interrupt_handler_address = 247 + masked_interrupts
+
+                # Disable further interrupts.
+                self.interrupts_enabled = False
+
+                # Clear the bit in the IS register.
+                # Reset Interrupt status to 0
+                self.register[self.interrupt_status] = 0
+
+                # The PC register is pushed on the stack.
+                self.register[self.sp] -= 1
+                SP = self.register[self.sp]
+                self.ram_write(self.pc, SP)
+
+                # The FL register is pushed on the stack.
+                self.register[self.sp] -= 1
+                SP = self.register[self.sp]
+                self.ram_write(self.FL, SP)
+                
+                # Registers R0-R6 are pushed on the stack in that order.
+                for j in range(7):
+                    self.register[self.sp] -= 1
+                    SP = self.register[self.sp]
+                    self.ram_write(self.register[j], SP)
+
+                # The address (vector in interrupt terminology) of the appropriate handler is looked up from the interrupt vector table.
+                # Set the PC is set to the handler address.
+                self.pc = self.ram[self.interrupt_handler_address]
+
+                # Stop further checking of maskedInterrupts.
+                break
+
     def handle_NOP(self):
-        print("THIS IS NOP")
+        print("THIS IS NOP. EXITING PROGRAM")
         sys.exit(1)
 
     def handle_PRA(self):
@@ -119,19 +207,14 @@ class CPU:
         set_pointer = self.ram_read(self.pc + 1)
         self.pc = set_pointer
 
-        pass
     def handle_IRET(self):
         '''
         Handle Interrupt return
+        Pop all items off the stack back to their original places.
+        End interrupt
         '''
-        # While an interrupt is being serviced (between the handler being called and the IRET), further interrupts are disabled.
-
-        # Return from an interrupt handler.
-        # The following steps are executed:
-
 
         # Registers R6-R0 are popped off the stack in that order.
-
         for i in range(6, -1, -1):
 
             SP = self.register[self.sp]
@@ -153,7 +236,6 @@ class CPU:
 
         # Interrupts are re-enabled
         self.interrupts_enabled = True
-        pass
 
     def handle_ST(self):
         '''
@@ -164,6 +246,15 @@ class CPU:
         reg_a_value = self.register[regA]
         reg_b_value = self.register[regB] 
         self.ram[reg_a_value] = reg_b_value
+        self.pc += 3
+    def handle_LD(self):
+        '''
+        Loads registerA with the value at the memory address stored in registerB.
+        '''
+        regA = self.ram[self.pc+1] # 0
+        regB = self.ram[self.pc + 2] # 1
+        reg_b_value = self.register[regB] 
+        self.register[regA] = self.ram[reg_b_value]
         self.pc += 3
     def handle_CALL(self):
         '''
@@ -304,24 +395,9 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        init_time = time.time()
-
+        self.init_time = time.time()
+        
         while True:
-
-            # Prior to instruction fetch, the following steps occur:
-
-            #DONE # The IM register is bitwise AND-ed with the IS register and the results stored as maskedInterrupts.
-            
-            #DONE # Each bit of maskedInterrupts is checked, starting from 0 and going up to the 7th bit, one for each interrupt.
-            
-            # TODO
-            # If a bit is found to be set, follow the next sequence of steps. 
-            
-            # DONE # Stop further checking of maskedInterrupts.
-            
-            '''
-            Add the timer interrupt to the LS-8 emulator
-            '''
 
             '''
             Interrupt numbers
@@ -329,53 +405,9 @@ class CPU:
             1: Keyboard interrupt. This interrupt triggers when a key is pressed. The value of the key pressed is stored in address 0xF4.
             '''
 
+            self.kbfunc()
             if self.interrupts_enabled:
-                current_time = time.time()
-                if current_time - init_time >=1:
-                    init_time = time.time() # Reset base time to check against
-
-                    # R6 is reserved for the interrupt_status
-                    self.register[self.interrupt_status] = 1 # Interrupt status is on? (0b00000001)
-
-                # If interrupt mask is on and interrupt status is on, this will evaluate to 1
-                masked_interrupts = self.register[self.interrupt_mask] & self.register[self.interrupt_status]
-
-                # Check if interrupt status is switched on
-                for i in range(8):
-                    # Right shift interrupts down by i, then mask with 1 to see if that bit was set
-                    interrupt_happened = ((masked_interrupts >> i) & 1) == 1
-
-                    # If a bit is set:
-                    if interrupt_happened:
-                        # Disable further interrupts.
-                        self.interrupts_enabled = False
-
-                        # Clear the bit in the IS register.
-                        # Reset Interrupt status to 0
-                        self.register[self.interrupt_status] = 0
-
-                        # The PC register is pushed on the stack.
-                        self.register[self.sp] -= 1
-                        SP = self.register[self.sp]
-                        self.ram_write(self.pc, SP)
-
-                        # The FL register is pushed on the stack.
-                        self.register[self.sp] -= 1
-                        SP = self.register[self.sp]
-                        self.ram_write(self.FL, SP)
-                        
-                        # Registers R0-R6 are pushed on the stack in that order.
-                        for j in range(7):
-                            self.register[self.sp] -= 1
-                            SP = self.register[self.sp]
-                            self.ram_write(self.register[j], SP)
-
-                        # The address (vector in interrupt terminology) of the appropriate handler is looked up from the interrupt vector table.
-                        # Set the PC is set to the handler address.
-                        self.pc = self.ram[self.interrupt_handler_address]
-
-                        # Stop further checking of maskedInterrupts.
-                        break
+                self.handle_interrupt()
 
             IR = self.ram[self.pc]
             self.branchtable[IR]()
